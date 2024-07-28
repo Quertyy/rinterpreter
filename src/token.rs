9e2ab20d4d3bdf330aa41adaa6ed26b136b00fa6
Quertyy <98064975+Quertyy@users.noreply.github.com>
2024-07-28 18:55:05 +0200

    feat: Token struct

---
+++ src/token.rs
@@ -0,0 +1,27 @@
+use super::token_type::TokenType;
+
+#[derive(Debug, Clone)]
+pub struct Token<'a> {
+    pub token_type: TokenType,
+    pub lexeme: &'a str,
+    pub litteral: String,
+    pub line: u64,
+}
+
+impl<'a> Token<'a> {
+    pub fn new(token_type: TokenType, lexeme: &'a str, litteral: String, line: u64) -> Self {
+        Self {
+            token_type,
+            lexeme,
+            litteral,
+            line,
+        }
+    }
+}
+
+impl<'a> std::fmt::Display for Token<'a> {
+
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} {}", self.token_type, self.lexeme, self.litteral)
+    }
+}
